5d4ab7dc2ca3ff108d8374591dc024aeb5dea495
Lili <102562771+lilimfobo@users.noreply.github.com>
2022-10-14 08:14:50 +0200

    Update movieList.js

--- js/movieList.js
+++ js/movieList.js
@@ -35,3 +35,25 @@ window.addEventListener('scroll' , () => {
         nav.classList.remove('nav_black');
     }
 })
+
+/* The movies that are displayed in the homepage, are shown here in the array to access and show the movies */
+[comingSoon],//This is all the information about the Movies and TV Shows sorted within an array collected from the imdb api (eg. the id, title, genre, date available, description, image and trailer.)
+            {   id: "Coming Soon", //Movie row name
+                title: "Love in the villa", //Title
+                genre: "Comedy, Romance", //Genre
+                comingSoon: true, //Boolean if the movie is: out=false, coming soon=true
+                availDate: 2022, //When the movie is available to watch
+                description: "A young woman takes a trip to romantic Verona, Italy, after a breakup, only to find that the villa she reserved was double-booked, and she'll have to share her vacation with a cynical British man.", //Description of the movie
+                image: "https://www.imdb.com/title/tt15463032/mediaviewer/rm3033009665/?ref_=tt_ov_i", //Image URL's
+                preview: "https://www.youtube.com/watch?v=_kRab62q54M",  //Trailer URL's
+            },
+            {
+                id: "Coming Soon", //Movie row name
+                title: "After ever happy", //Title
+                genre: "Romance, Drama", //Genre
+                comingSoon: true, //Boolean if the movie is: out=false, coming soon=true
+                availDate: 2022, //When it is available to watch
+                description: "As a shocking truth about a couple's families emerges, the two lovers discover they are not so different from each other. Tessa is no longer the sweet, simple, good girl she was when she met Hardin — any more than he is the cruel, moody boy she fell so hard for.", //Description of the movie
+                image: "https://www.imdb.com/title/tt13070038/mediaviewer/rm1802494977/?ref_=tt_ov_i", //Image URL's
+                preview: "https://www.youtube.com/watch?v=TlAukkPK-dc",  //Trailer URL's
+            },
