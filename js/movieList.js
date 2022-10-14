955d9f835ae7b696cce17071431ca599e9f24d62
Lili <102562771+lilimfobo@users.noreply.github.com>
2022-10-14 08:14:11 +0200

    Update movieList.js

--- js/movieList.js
+++ js/movieList.js
@@ -25,3 +25,13 @@ function changeButtonType(btn,value){
     btn.title - value;
     btn.innerHTML - value;
 }
+
+/* Functionailty for the media scrollers */
+const nav = document.getElementById('nav');
+window.addEventListener('scroll' , () => {
+    if(window.scrollY>=100){
+        nav.classList.add('nav_black');
+    } else{
+        nav.classList.remove('nav_black');
+    }
+})
